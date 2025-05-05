247ebdbf112b4f75eca035186dd41f6cfc94c9c6
Tuan Diniz <tuandm@gmail.com>
2025-05-05 07:02:17 -0700

    Update rectangles example

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -9,13 +9,15 @@ impl Rectangle {
         self.width * self.height
     }
     
-    fn fits_into(&self, other: &Rectangle) -> bool {
-        if self.height <= other.height &&
-            self.width <= other.width {
-            return true;
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+    
+    fn square(size: u32) -> Self {
+        Self {
+            height: size,
+            width: size
         }
-        
-        false
     }
 }
 
@@ -27,13 +29,22 @@ fn main() {
     
     let rect2 = Rectangle {
         width: dbg!(3 * 13),
-        ..rect
+        height: 40
+    };
+    
+    let rect3 = Rectangle {
+        width: 20,
+        height: 62
     };
     
     println!(
         "The area of the rectangle rect is {} square pixels", rect.area()
     );
-    println!("Rec fits Rec1? Answer = {}", rect.fits_into(&rect2));
+    println!("Rec can hold Rec1? Answer = {}", rect.can_hold(&rect2));
+    println!("Rec can hold Rec3? Answer = {}", rect.can_hold(&rect3));
     
     dbg!(rect);
+    
+    let square = Rectangle::square(40);
+    println!("A square: {:?}, and area: {}", square, square.area());
 }
